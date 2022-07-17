b641abf7f4d9996ea5cde7495b07bb5d43660254
Arkadiy <arickmail1997@gmail.com>
2022-07-17 15:57:06 +0300

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/number_masked_input/',
   resolve:{
     alias:{
       '@' : path.resolve(__dirname, './src')
